c95fb9b964a61c2f747c0eb2626312c3bbb1111c
buithien <thien7278@gmail.com>
2023-05-04 18:33:58 +0700

    UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,10 +1,27 @@
 import classNames from 'classnames/bind';
 import style from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(style); // có thể viết css có dấu -
 function Header() {
    return (
       <header className={cx('wrapper')}>
-         <div className={cx('inner')}>{/* search */}</div>
+         <div className={cx('inner')}>
+            <div className={cx('logo')}>
+               <img src={images.logo} alt="TikTok" />
+            </div>
+            <div className={cx('search')}>
+               <input placeholder="Search accounts and videos" spellCheck={false} />
+               <button className={cx('clear')}>
+                  <FontAwesomeIcon icon={faCircleXmark} />
+               </button>
+               <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+               <button className={cx('search-btn')}>
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+               </button>
+            </div>
+         </div>
       </header>
    );
 }
